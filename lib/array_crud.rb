8ceb0c78b7e994b0b53657536ed095ec8c7dacfd
keemo <keemowatson1@gmail.com>
2019-11-08 22:32:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,9 +14,9 @@ def add_element_to_start_of_array(array, element)
 add_element_to_start_of_array = [array].unshift("#{element}")
 end
 
-def remove_element_from_end_of_array(array)
-remove_element_from_end_of_array = [array]
-remove_element_from_end_of_array.pop(0)
+def remove_element_from_end_of_array(element, array)
+remove_element_from_end_of_array = [element, array]
+remove_element_from_end_of_array.pop
 end
 
 def remove_element_from_start_of_array(array)
